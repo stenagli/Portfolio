4d2a322c622bff3cc2e9e4a445abbeb0b8d7e8ce
Steven Tenaglia <stenten@gmail.com>
2016-06-05 13:21:12 -0400

    finished preliminary merge code

--- merge_sort.c
+++ merge_sort.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
+#define A_SIZE 8
+
+/* function prototype */
+void merge(int A[], int p, int q, int r);
+
 int main(){
-int A[10] = {3, 2, 1};
-printf("%d\n",add(INT_MAX,1));
-return 0;
+  int A[A_SIZE] = {2, 4, 5, 7, 1, 2, 3, 6};
+
+/*
+for(int i = 0; i < A_SIZE; i++)
+	printf("%d\n",A[i]);
+*/
+
+  merge(A, 0, 3, 7);
+
+  return 0;
 }
 
 
-int add(int i, int j){
-return i+j;
+void merge(int A[], int p, int q, int r){
+	int n1 = q-p+1;
+	int n2 = r-q;
+	int L[n1], R[n2];
+
+	int *Ap = A;
+	int *Lp = L;
+	int *Rp = R;
+
+	/* populate left and right arrays */
+	for(int i = 0; i < n1; i++)
+		L[i] = A[p+i];
+	for(int i = 0; i < n2; i++)
+		R[i] = A[q+i+1];
+
+	/* start merge */
+	int i = 0;
+        int j = 0;
+	printf("%d, %d\n", i, j);
+
 }
+
