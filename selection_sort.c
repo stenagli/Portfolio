ee9bbc3d4a1bae39e0fceaa629d1552b28c0d27d
Steven Tenaglia <stenten@gmail.com>
2016-06-06 14:45:46 -0400

    selection_sort v1.0

--- selection_sort.c
+++ selection_sort.c
@@ -11,7 +11,14 @@ int main(){
 
 	int idx_s = find_smallest(A, 0, A_SIZE-1);
 	printf("Index of smallest: %d\nValue at smallest: %d\n", idx_s, A[idx_s]);
+
+	selection_sort(A, A_SIZE);
+
+	for(int i = 0; i < A_SIZE-1;i++)
+		printf("%d, ",A[i]);
+	printf("%d\n", A[A_SIZE-1]);
 	return 0;
+
 }
 
 void selection_sort(int A[], int length){
