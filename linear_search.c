b5da972f4247125eedebe62431ab19996bacd8f4
Steven Tenaglia <stenten@gmail.com>
2016-06-06 11:52:19 -0400

    linear_search v1.0

---
+++ linear_search.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define A_SIZE 8
+#define VALUE 4
+
+/* function prototypes */
+int linear_search(int A[], int length, int value);
+
+int main(){
+	int A[A_SIZE] = {8, 6, 4, 2, 7, 5, 3, 1};
+	int index = linear_search(A, A_SIZE, VALUE);
+	printf("Index of value %d: %d\n", VALUE, index);
+	printf("Value at that index: %d\n", A[index]);
+}
+
+
+int linear_search(int A[], int length, int value){
+	/* returns index of 'value', or -1 if not present */
+	length --;
+	while(length >= 0){
+		if (A[length] == value)
+			return length;
+		length--;
+	}
+	return -1;
+}
