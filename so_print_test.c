dbeac237bfc0959abdab73ef712e228b4a13a5e5
Steven Tenaglia <stenten@gmail.com>
2016-06-14 17:33:29 -0400

    Initial shared object test

---
+++ so_print_test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "so_print_test.h"
+
+void print_test(void){
+	printf("This is printing from so_print_test library");
+}
---
+++ so_test_main.c
@@ -0,0 +1,8 @@
+#include "so_print_test.h"
+
+int main(){
+
+	print_test();
+
+	return 0;
+}
