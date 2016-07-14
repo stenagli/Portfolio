5cac4231ef27eae6ee1b190a3f603673c5602685
Steven Tenaglia <stenten@gmail.com>
2016-07-14 17:17:49 -0400

    Created print_datatypes.c

---
+++ print_datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	printf("Size of char: %zu bytes\n", sizeof(char));
+	printf("Size of short: %zu bytes\n", sizeof(short));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of long: %zu bytes\n", sizeof(long));
+	printf("Size of long long: %zu bytes\n", sizeof(long long));
+	printf("Size of char *: %zu bytes\n", sizeof(char *));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	printf("Size of double *: %zu bytes\n", sizeof(double));
+}
