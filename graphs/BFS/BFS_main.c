54da25bf806e0e2783fb38cfe4673146324ee391
Steven Tenaglia <stenten@gmail.com>
2016-08-08 16:47:10 -0400

    Initial Breadth First Search commit

---
+++ graphs/BFS/BFS_main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct vertex_list_element {
+	int vertex;
+	vertex_list_element next;
+}
+
+vertex_list_element *create_graph(void){
+	
+
+}
+
+int main(void){
+	vertex_list_element *Adj = create_graph();
+
+}
