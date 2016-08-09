ab86e118f8d669631706fac3e0fcb003f9044487
Steven Tenaglia <stenten@gmail.com>
2016-08-09 11:08:25 -0400

    Bugfixes in Makefile and main file

--- graphs/BFS/BFS_main.c
+++ graphs/BFS/BFS_main.c
@@ -4,7 +4,20 @@
 struct vertex_list_element {
 	int vertex;
 	struct vertex_list_element *next;
+};
+
+
+void add_edge(struct vertex_list_element *Adj[], int from, int to){
+	if (Adj[from] == NULL) {
+		struct vertex_list_element *v = {to, NULL};
+		Adj[from] = v;
+	}
+	else { /* Create new vertex and add to beginning of list */
+		struct vertex_list_element *v= {to, Adj[from]};
+		Adj[from] = v;
+	}
 }
+
 
 struct vertex_list_element **create_graph(void){
 	/* Create Adj list and initialize to NULL */
@@ -18,16 +31,6 @@ struct vertex_list_element **create_graph(void){
 	return Adj;
 }
 
-void add_edge(struct vertex_list_element *Adj[], int from, int to){
-	if (Adj[from] == NULL) {
-		struct vertex_list_element *v = {to, NULL};
-		Adj[from] = v;
-	}
-	else { /* Create new vertex and add to beginning of list */
-		struct vertex_list_element *v= {to, Adj[from]};
-		Adj[from] = v;
-	}
-}
 
 void print_graph(struct vertex_list_element *Adj[]){
 	for (int i = 0; i < NUM_VERTICES; i++){
@@ -38,9 +41,11 @@ void print_graph(struct vertex_list_element *Adj[]){
 			printf(" %d", v->vertex); 
 			v = v->next;
 		}
-		printf("\n";
+		printf("\n");
 	}
 }
+
+
 int main(void){
 	struct vertex_list_element **Adj = create_graph();
 
