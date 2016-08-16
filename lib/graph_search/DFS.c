48912bb11b84aa77f65be23619fc54065ceef986
Steven Tenaglia <stenten@gmail.com>
2016-08-16 13:31:16 -0400

    Created visited and distance variables for DFS

--- lib/graph_search/DFS.c
+++ lib/graph_search/DFS.c
@@ -2,4 +2,9 @@
 #include "../data_structures/data_structures.h"
 #include <stdio.h>
 
+void DSF(struct vertex_list_element **Adj, int source){
 
+	char visited = 0; /* Bitmap denoting whether a vertex has been seen or not */
+
+	int distance[NUM_Vertices]; /* Distance of each vertex from the source */
+}
