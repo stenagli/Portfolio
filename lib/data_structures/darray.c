05ad42f1adf72add31acdd5b97ec41f071ab37bd
Steven Tenaglia <stenten@gmail.com>
2016-08-17 19:37:39 -0400

    Dynamic array implementation confirmed working

--- lib/data_structures/darray.c
+++ lib/data_structures/darray.c
@@ -19,7 +19,7 @@ void push(struct darray *darray, int key){
 			darray->array = malloc(sizeof((darray->array)[0]));
 			if(darray->array == NULL){
 				/* If malloc fails, darray is unchanged */
-				fprintf(stderr,"Unable to push element %d: expanding empty array failed", key);
+				fprintf(stderr,"Unable to push element %d: expanding empty array failed\n", key);
 				return;
 			}
 			(darray->size)++;
@@ -28,7 +28,7 @@ void push(struct darray *darray, int key){
 			int *new_array = realloc(darray->array, 2*(darray->size)*sizeof((darray->array)[0]));
 			if (new_array == NULL){
 				/* Use the old pointer, and indicate that pushing failed */
-				fprintf(stderr,"Unable to push element %d: expanding nonempty array failed", key);
+				fprintf(stderr,"Unable to push element %d: expanding nonempty array failed\n", key);
 				return;
 			}
 			darray->array = new_array;
@@ -42,7 +42,7 @@ void push(struct darray *darray, int key){
 
 int pop(struct darray *darray){
 	if (darray->num_elements == 0){
-		fprintf(stderr,"Attempted to pop empty array");
+		fprintf(stderr,"Attempted to pop empty array\n");
 		return -1;
 	}
 
@@ -55,7 +55,7 @@ int pop(struct darray *darray){
 
 	if (darray->array == NULL && darray->num_elements != 0){
 		/* An error occurred, and should be reported */
-		fprintf(stderr,"Array contraction error: null pointer returned but array is not empty");
+		fprintf(stderr,"Array contraction error: null pointer returned but array is not empty\n");
 		return -1;
 	}
 
@@ -66,4 +66,17 @@ int pop(struct darray *darray){
 void delete_darray(struct darray *darray){
 	free(darray->array);
 	free(darray);
+}
+
+
+int main(void){
+	struct darray *darray = init_darray();
+	printf("Popped %d\n", pop(darray));
+	push(darray,1);
+	push(darray,2);
+	push(darray,3);
+	for (int i = 0; i < 4; i++)
+		printf("Popped %d\n", pop(darray));
+	delete_darray(darray);
+	return 0;
 }
